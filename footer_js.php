d69850c065df0062f53c22702db357c95ab344fe
TradeSis Helio <helio@tradesis.com.br>
2024-07-02 16:15:35 -0300

    versao inicial

---
+++ footer_js.php
@@ -0,0 +1,20 @@
+
+        <!-- Bootstrap Atualizado -->
+        <script src="<?php echo URLROOT ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- jquery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <!-- input dos formularios -->
+        <script src="<?php echo URLROOT ?>/sistema/js/input.js"></script>
+        <!-- QUILL -->
+        <script src="<?php echo URLROOT ?>/vendor/quilljs/quill.min.js"></script>
+        <!-- JQUERY MASK -->
+        <script src="<?php echo URLROOT ?>/vendor/jquery/jquery.mask.min.js"></script>
+        
+
+
+      
+
+        
+
+
+        
---
+++ head_css.php
@@ -0,0 +1,31 @@
+<?php
+include_once __DIR__ . "/../config.php";
+?>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Icone da Pagina -->
+        <link rel="icon" href="<?php echo URLROOT ?>/img/meucontrole_icon.png" type="image/png">
+
+        <!-- Bootstrap Atualizado -->
+        <link href="<?php echo URLROOT ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <!---Bootstrap Icon -->
+        <link rel="stylesheet" href="<?php echo URLROOT ?>/vendor/bootstrap/bootstrap-icons-1.11.3/font/bootstrap-icons.css">
+
+        <!-- CSS -->
+        <link href="<?php echo URLROOT ?>/sistema/css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="<?php echo URLROOT ?>/sistema/css/estilo.css" rel="stylesheet" type="text/css">
+ 
+        
+        <!-- Fonte Inter Helio 03102023 SITE https://fonts.google.com/specimen/Inter -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <!-- Fonte Inter -->
+       
+        <!-- QUILL -->
+        <link href="<?php echo URLROOT ?>/vendor/quilljs/quill.snow.css" rel="stylesheet">
+
+
+        
+ 
