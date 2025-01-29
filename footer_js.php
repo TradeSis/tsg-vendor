6ace09f983c87c822aee199aa11567bac8be8fe9
tradesishelio <99908450+tradesishelio@users.noreply.github.com>
2025-01-29 15:37:38 -0300

    ptoteção

--- footer_js.php
+++ footer_js.php
@@ -15,7 +15,45 @@
         <script src="<?php echo URLROOT ?>/sistema/js/Table.js"></script> 
         
 
+        <script>
 
+//Desativa F12 e Clique direito
+
+$(document).keydown(function (event)
+    {
+        if (event.keyCode == 123)
+        { // Prevent F12
+            return false;
+        }
+        else if (event.ctrlKey && event.shiftKey && event.keyCode == 73)
+        { // Prevent Ctrl+Shift+I
+            return false;
+        }
+    });
+
+ $(document).on("contextmenu", function (e)
+    {
+        e.preventDefault();
+    });
+    
+/** TO DISABLE SCREEN CAPTURE **/
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'PrintScreen') {
+        navigator.clipboard.writeText('');
+        alert('Screenshots disabled!');
+    }
+});
+
+/** TO DISABLE PRINTS WHIT CTRL+P **/
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key == 'p') {
+        alert('This section is not allowed to print or export to PDF');
+        e.cancelBubble = true;
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }
+});
+        </script>
       
 
         
